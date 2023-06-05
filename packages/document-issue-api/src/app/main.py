6f68d6aa559b50f53f7d8f889c29f59b84d7c531
jgunstone <gunstone.john@gmail.com>
2023-06-05 10:17:34 +0100

    added project and project_role

--- packages/document-issue-api/src/app/main.py
+++ packages/document-issue-api/src/app/main.py
@@ -7,6 +7,8 @@ from fastapi import FastAPI
 
 import app.issue.main as issue_main
 import app.role.main as role_main
+import app.project.main as project_main
+import app.project_role.main as project_role_main
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +31,5 @@ app = FastAPI(
 )
 app.include_router(issue_main.router)
 app.include_router(role_main.router)
+app.include_router(project_main.router)
+app.include_router(project_role_main.router)
