6269f77f474c01b9e01f6a0722834a5429e5dc3e
jgunstone <gunstone.john@gmail.com>
2023-06-23 16:42:02 +0100

    added simple tests to `document-issue`

--- packages/document-issue/tests/test_document.py
+++ packages/document-issue/tests/test_document.py
@@ -1,8 +1,7 @@
-from document_issue.document_new import Document
-from document_issue.project import ProjectBase
-from obj_funcs import create_project_base
+from document_issue.document import Document
+from obj_funcs import create_project
 
 
 def test_Document():
-    doc = Document(project=create_project_base())
+    doc = Document(project=create_project())
     assert doc.document_code == "06667-MXF-XX-XX-SH-M-20003"
--- packages/document-issue/tests/test_project.py
+++ packages/document-issue/tests/test_project.py
@@ -1,7 +1,7 @@
 from document_issue.project import ProjectBase
-from obj_funcs import create_project_base
+from obj_funcs import create_project
 
 
 def test_ProjectBase():
-    proj = create_project_base()
+    proj = create_project()
     assert proj.project_number == 6667
