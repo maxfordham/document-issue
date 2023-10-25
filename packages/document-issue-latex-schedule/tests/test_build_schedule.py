27d536fc787ef863f2ee34fab116fb08f460d911
ollyhensby <o.hensby@maxfordham.com>
2023-10-25 09:19:15 +0100

    🎨 Restructured into quarto extension

--- packages/document-issue-latex-schedule/tests/test_build_schedule.py
+++ packages/document-issue-latex-schedule/tests/test_build_schedule.py
@@ -1,13 +1,25 @@
 import os
+import shutil
 import pathlib
 import subprocess
 
-FDIR_TESTS = pathlib.Path(__file__).parent
+FDIR_ROOT = pathlib.Path(__file__).parent.parent
+FDIR_TESTS = FDIR_ROOT / "tests"
 FDIR_TESTDATA = FDIR_TESTS / "testdata"
 
-def test_build_schedule():
-    FDIR_OUTPUT = FDIR_TESTDATA / "document.pdf"
-    FDIR_OUTPUT.unlink(missing_ok=True)
+
+def test_install_extension():
+    FDIR_EXTENSION_INSTALL_PTH = FDIR_TESTDATA / "_extensions" / "document-issue-schedule"
+    if FDIR_EXTENSION_INSTALL_PTH.exists():
+        shutil.rmtree(FDIR_EXTENSION_INSTALL_PTH)
     os.chdir(FDIR_TESTDATA)
-    subprocess.run(["quarto", "render", "document.md", "--to", "pdf"])
-    assert FDIR_OUTPUT.exists()
+    subprocess.run(["quarto", "add", str(FDIR_ROOT), "--no-prompt"])
+    assert FDIR_EXTENSION_INSTALL_PTH.exists()
+
+def test_build_schedule():
+    FPTH_OUTPUT = FDIR_TESTDATA / "document.pdf"
+    FPTH_OUTPUT.unlink(missing_ok=True)
+    os.chdir(FDIR_TESTDATA)
+    subprocess.run(["quarto", "add", str(FDIR_ROOT), "--no-prompt"])
+    subprocess.run(["quarto", "render", "document.md", "--to", "document-issue-schedule-pdf"])
+    assert FPTH_OUTPUT.exists()
