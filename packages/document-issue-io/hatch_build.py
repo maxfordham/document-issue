b00a475834c58e1cb0bf330d6f81b875bd7e713e
Oliver Hensby <o.hensby@maxfordham.com>
2023-10-26 16:18:01 +0100

    🔧 Add hatch build hook

---
+++ packages/document-issue-io/hatch_build.py
@@ -0,0 +1,39 @@
+import os
+import pathlib
+import subprocess
+import shutil
+from contextlib import contextmanager
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+FDIR_DOCUMENT_ISSUE_QUARTO = pathlib.Path(__file__).parents[1] / "document-issue-quarto"
+FDIR_TEMPLATES = (
+    pathlib.Path(__file__).parent / "src" / "document_issue_io" / "templates"
+)
+
+
+@contextmanager
+def change_dir(directory):
+    current_dir = os.getcwd()
+    os.chdir(directory)
+    try:
+        yield
+    finally:
+        os.chdir(current_dir)
+
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        """Build the document-issue-quarto package and move the tarball to the
+        document-issue-io package into the templates directory."""
+        tar_name = "document-issue-quarto.tar.gz"
+        with change_dir(FDIR_DOCUMENT_ISSUE_QUARTO):
+            subprocess.run(
+                [
+                    "tar",
+                    "cvzf",
+                    tar_name,
+                    "_extensions",
+                ]
+            )
+            shutil.move(tar_name, FDIR_TEMPLATES / tar_name)
