e58b114493ad08b85bde64a4dec36da3d4c02770
Oliver Hensby <o.hensby@maxfordham.com>
2024-04-26 12:03:43 +0100

    🐛 Fix StatusEnum validation error

--- packages/document-issue/tests/test_document_issue.py
+++ packages/document-issue/tests/test_document_issue.py
@@ -1,8 +1,16 @@
 from document_issue.document_issue import Issue
+from document_issue.issue import StatusRevisionEnum
+
 
 def test_Issue():
     issue = Issue()
     assert issue.revision == "P01"
     di = issue.model_dump(mode="json")
     assert di["revision"] == "P01"
-    
+
+
+def test_StatusRevisionEnum():
+    """Test that all StatusRevisionEnum values are valid."""
+    for enum in StatusRevisionEnum:
+        issue = Issue(status_revision=enum)
+        assert issue
