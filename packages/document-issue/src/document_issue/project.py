d65e8932d0a37fa121c8d67e958862fe9e6cd197
Oliver Hensby <o.hensby@maxfordham.com>
2024-05-02 10:12:03 +0100

    🦺 Update project number validation Deal with project numbers without the prefix "J" If incorrect value then return default project number

--- packages/document-issue/src/document_issue/project.py
+++ packages/document-issue/src/document_issue/project.py
@@ -2,22 +2,30 @@
 define base characteristics of a Project
 """
 
+import logging
 import typing as ty
 from document_issue.constants import DEFAULT_PROJECT_NUMBER
 from document_issue.basemodel import BaseModel, Field
 from pydantic import BeforeValidator
 
 
-def is_job_number_string(s):
-    return isinstance(s, str) and "J" in s and len(s) == 5
+def validate_project_number(v):
+    """Validate project number. If it starts with 'J', remove it and convert to int.
+    If it is not a number, log a warning and use the default project number."""
+    try:
+        if isinstance(v, str) and v.startswith("J"):
+            return int(v.replace("J", ""))
+        else:
+            return int(v)
+    except ValueError:
+        logging.warning(f"Invalid project number: {v}. Use default.")
+        return DEFAULT_PROJECT_NUMBER
 
 
 class ProjectBase(BaseModel):
     project_number: ty.Annotated[
         int,
-        BeforeValidator(
-            lambda v: int(v.replace("J", "")) if is_job_number_string(v) else v
-        ),
+        BeforeValidator(validate_project_number),
     ] = Field(DEFAULT_PROJECT_NUMBER, description="unique number project code")
     project_name: str = Field(
         "In House App Testing", description="should be the same as the WebApp"
