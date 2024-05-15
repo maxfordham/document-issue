03d2be81315ad8272d043513b8bd7e82158a6150
Oliver Hensby <o.hensby@maxfordham.com>
2024-05-15 16:49:02 +0100

    ♻️ Refactor document_issue_md_to_pdf This simplifies the code as now we can just pass any markdown file to the function

--- packages/document-issue-io/src/document_issue_io/utils.py
+++ packages/document-issue-io/src/document_issue_io/utils.py
@@ -1,11 +1,8 @@
 import os
 import pathlib
 import subprocess
-import shutil
 from contextlib import contextmanager
 
-from document_issue.document_issue import DocumentIssue
-from document_issue_io.title_block import build_schedule_title_page_template_pdf
 from document_issue_io.constants import FDIR_TEMPLATES, FDIR_MEDIA
 
 FPTH_INSTALL_TAR = FDIR_TEMPLATES / "document-issue-quarto.tar.gz"
@@ -36,31 +33,3 @@ def install_or_update_document_issue_quarto_extension():
         install_document_issue_quarto_extension()
     else:
         update_document_issue_quarto_extension()
-
-
-def document_issue_md_to_pdf(
-    document_issue: DocumentIssue, fpth_md: pathlib.Path, fpth_pdf: pathlib.Path
-):
-    """Convert markdown document issue to pdf using quarto."""
-    with change_dir(fpth_md.parent):
-        shutil.copy(
-            FPTH_FOOTER_LOGO, FPTH_FOOTER_LOGO.name
-        )  # Copy footer logo to markdown document issue directory
-        build_schedule_title_page_template_pdf(document_issue=document_issue)
-        install_or_update_document_issue_quarto_extension()
-        subprocess.run(
-            [
-                "quarto",
-                "render",
-                fpth_md.name,
-                "--to",
-                "document-issue-pdf",
-                "-o",
-                fpth_pdf.name,
-            ]
-        )
-        # NOTE: quarto render does not allow to specify a relative or absolute
-        # path for "-o" parameter. Therefore, will just move post-render
-        if fpth_pdf != fpth_md.parent / fpth_pdf.name:
-            shutil.move(fpth_pdf.name, fpth_pdf)
-    return fpth_pdf
