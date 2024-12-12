7ddba6d379199e916f8a910fefa982d119a02683
Oliver Hensby <o.hensby@maxfordham.com>
2024-12-12 18:21:38 +0000

    ✅ Temporary test for creating landscape plantroom schedule

--- packages/document-issue-quarto/tests/test_build_schedule.py
+++ packages/document-issue-quarto/tests/test_build_schedule.py
@@ -32,6 +32,20 @@ def test_build_schedule():
     )  # log file should be deleted if Quarto PDF compilation is successful
 
 
+def test_build_schedule_landscape():
+    FDIR_TESTDATA = FDIR_TESTS / "test-landscape"
+    FPTH_OUTPUT = FDIR_TESTDATA / "document.pdf"
+    FPTH_OUTPUT.unlink(missing_ok=True)
+    os.chdir(FDIR_TESTDATA)
+    # subprocess.run(["quarto", "add", str(FDIR_ROOT), "--no-prompt"])  # NOTE: Comment out for now as this test has custom landscape and A3 settings
+    subprocess.run(["quarto", "render", "document.md", "--to", "document-issue-pdf"])
+    assert FPTH_OUTPUT.exists()
+    FPTH_LOG = FDIR_TESTDATA / "document.log"
+    assert (
+        not FPTH_LOG.exists()
+    )  # log file should be deleted if Quarto PDF compilation is successful
+
+
 def test_build_examples():
     EXAMPLE_DIRS = [
         i
@@ -62,6 +76,7 @@ def test_build_quarto_yaml():
     assert FPTH_OUTPUT.exists()
     assert not FPTH_LOG.exists()
 
+
 def test_build_document_properties():
     FDIR = FDIR_ROOT / "examples" / "document-properties"
     FPTH_INPUT = FDIR / "document.md"
@@ -73,4 +88,4 @@ def test_build_document_properties():
     assert not FPTH_LOG.exists()
 
     checked_props = check_quarto_doc_properties(FPTH_INPUT, FPTH_OUTPUT)
-    assert all(item in ['title', 'author', 'subject'] for item in checked_props)
+    assert all(item in ["title", "author", "subject"] for item in checked_props)
