c724e16f68372f1d423f319c8f78519738edc732
jgunstone <j.gunstone@maxfordham.com>
2024-12-27 18:19:03 +0000

    add `pytest-examples` with dummy example

--- packages/document-issue-io/tests/test.md
+++ packages/document-issue-io/tests/test.md
@@ -1,9 +1,9 @@
 # How `add_two_things` works
 
 ```py
-from document_issue_io import add_two_things
+from document_issue_io.constants import LONDON_ADDRESS
 
-print(add_two_things(1, 4))
-#> 5
+print(LONDON_ADDRESS[3])
+#> NW1 7PE
 ```
 
--- packages/document-issue-io/tests/test_examples.py
+++ packages/document-issue-io/tests/test_examples.py
@@ -2,7 +2,7 @@ import pytest
 from pytest_examples import find_examples, CodeExample, EvalExample
 
 
-@pytest.mark.parametrize('example', find_examples('.'), ids=str)
+@pytest.mark.parametrize('example', find_examples('tests'), ids=str)
 def test_readme(example: CodeExample, eval_example: EvalExample):
     if eval_example.update_examples:
         eval_example.format(example)
