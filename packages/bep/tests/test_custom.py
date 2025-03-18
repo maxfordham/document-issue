8599649ccf4dd430fed3b3b21b49fdf4f700a4b6
jgunstone <j.gunstone@maxfordham.com>
2025-03-18 09:36:35 +0000

    refactor

--- packages/bep/tests/test_custom.py
+++ packages/bep/tests/test_custom.py
@@ -1,16 +1,13 @@
-
 import os
-import importlib
 import pathlib
-
 
 fdir = pathlib.Path(__file__).parent
 fpth_status_revision = fdir.joinpath("data/status_revision.csv")
-import os
+
 os.environ["PROJECT_CONFIGURATION_STATUS_REVISION"] = str(fpth_status_revision)
 
 def test_custom():
-    
+
     import bep
     STATUS_REVISION = bep.get_status_revision()
-    STATUS_REVISION == fpth_status_revision
+    fpth_status_revision == STATUS_REVISION
