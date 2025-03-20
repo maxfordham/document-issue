ba6b9cf1cb40d195bf88496a870ff8417f1a7ffc
jgunstone <j.gunstone@maxfordham.com>
2025-03-20 14:55:38 +0000

    TODO @ollyhensby - add mfdb

--- packages/bep/src/bep/default.py
+++ packages/bep/src/bep/default.py
@@ -1,9 +1,22 @@
-from .models import StatusRevisionTable, read_csv_records
 from importlib.resources import files
+import mysql.connector
+from .models import ProjectRoleTable, StatusRevisionTable, read_csv_records
+# from mfdb import get_project_roles #  TODO
 
 DIR_DEFAULT = files("bep.data")
 PTH_STATUS_REVISION_TABLE = DIR_DEFAULT.joinpath("status_revision.csv")
+PTH_PROJECT_ROLES = DIR_DEFAULT.joinpath("project_roles.csv")
+
 
 def get_default_status_revision_table() -> StatusRevisionTable:
     """Get the default status revision table."""
     return read_csv_records(PTH_STATUS_REVISION_TABLE, StatusRevisionTable)
+
+
+def get_default_project_roles():
+    """Get the default project roles. Try to read from database, if not available, read from csv."""
+    try:
+        pass
+        # return ProjectRoleTable(get_project_roles())  #  TODO
+    except mysql.connector.errors.DatabaseError:
+        return read_csv_records(PTH_PROJECT_ROLES, ProjectRoleTable)
