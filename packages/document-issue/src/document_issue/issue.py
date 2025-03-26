28b563bac9890688977a418ad857458a7874ffa0
Olly Hensby <79875276+ollyhensby@users.noreply.github.com>
2025-03-26 12:14:32 +0000

    ♻ Support different status revision (#118)
    * 🐛 Set maxsplit to 4 for status_revision
    This is so that if any dashes are used in the status_revision description, they are not split
    
    * 🦺 Relax status_revision validation to be a string
    In its current setup, the API will reject any new values that are not in the default enumeration.
    By setting to string, we can bypass this and the validation will be done on the frontend.
    
    * 👷 Bump setup-pixi to v0.8.3
    
    * 🔧 Update pixi.lock
    
    * 👷 Bump pixi-version in setup-pixi
    
    * 🦺 Add relaxed validation to check maximum number of dashes

--- packages/document-issue/src/document_issue/issue.py
+++ packages/document-issue/src/document_issue/issue.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 import datetime
 from enum import Enum
+from typing import Annotated
 
-from pydantic import Field, field_validator, model_validator
+from pydantic import AfterValidator, Field, field_validator, model_validator
 
 from document_issue.basemodel import BaseModel
 from document_issue.constants import COL_WIDTH
@@ -11,6 +12,18 @@ from document_issue.enums import IssueFormatEnum, StatusRevisionEnum
 
 description_author = "the person who authored the work."
 description_checked_by = "the person who checked the work."
+
+def _check_status_revision(v: str) -> str:
+    max_dashes = 4
+    if v.count(" - ") < max_dashes:
+        msg = "status_revision must be in the format 'code - status description - revision code - revision description - description'"
+        raise ValueError(msg)
+    return v
+
+StatusRevision = Annotated[
+    str,
+    AfterValidator(_check_status_revision),
+]
 
 # TODO: who issued to?
 first_status_revision = next(iter(StatusRevisionEnum))
@@ -24,10 +37,10 @@ class Issue(BaseModel):
         title="Date",
         json_schema_extra={"column_width": COL_WIDTH},
     )
-    status_revision: StatusRevisionEnum = Field(
-        first_status_revision,
+    status_revision: StatusRevision = Field(
+        first_status_revision.value,
         title="Status Revision Selector",
-        json_schema_extra={"column_width": 1},
+        json_schema_extra={"enum": [e.value for e in StatusRevisionEnum], "column_width": 1},
     )
     revision: str = Field(
         "",
@@ -104,7 +117,7 @@ class Issue(BaseModel):
             revision_code,
             revision_description,
             description,
-        ) = status_revision.split(" - ")
+        ) = status_revision.split(" - ", 4)
         self.revision = f"{revision_code}{str(self.revision_number).zfill(2)}"
         self.status_description = description
         return self
